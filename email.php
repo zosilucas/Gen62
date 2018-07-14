b19eb293c180218bbef6a0ba2124fdc01eff5c46
lucaszosi <lucas.zosi@slot1.com.ar>
2018-07-14 20:16:40 -0300

    Changes

--- email.php
+++ email.php
@@ -1,17 +1,27 @@
 <?php
 
-//$to      = 'arquitectos@gen62.com.ar';//$_REQUEST['inputEmail'];
-$to = 'zosi.lucas@gmail.com'; //$_REQUEST['inputEmail'];
-$emailFrom = $_REQUEST['inputEmail'];
-$subject = 'Recibiste un mensaje nuevo';
-$message = 'Nombre : ' . $_REQUEST['inputName'] . "\n";
-$message .= 'Email : ' . $emailFrom . "\n";
-$message .= 'Telefono : ' . $_REQUEST['inputPhone'] . "\n";
-$message .= 'Mensaje : ' . $_REQUEST['inputMessage'];
-$headers = 'From: ' . $emailFrom;
+$subject = 'Recibiste un mensaje de la pagina web'; // Subject of your email
+$to = 'zosi.lucas@gmail.com';  //Recipient's E-mail
+$emailTo = $_REQUEST['inputEmail'];
 
-$success = mail($to, $subject, $message, $headers);
+$headers = "MIME-Version: 1.1";
+$headers .= "Content-type: text/html; charset=iso-8859-1";
+$headers .= "From: " . $emailTo . "\r\n"; // Sender's E-mail
+$headers .= "Return-Path:" . $emailTo;
 
-return $success;
+$message .= 'Nombre: ' . $_REQUEST['inputName'] . "\n";
+$message .= 'Email: ' . $_REQUEST['inputEmail'] . "\n";
+$message .= 'Telefon : ' . $_REQUEST['inputPhone'] . "\n";
+$message .= 'Mensaje: ' . $_REQUEST['inputMessage'];
+$message = wordwrap($mensaje, 70, "\r\n");
 
+if (@mail($to, $subject, $message, $headers)) {
+    // Transfer the value 'sent' to ajax function for showing success message.
+    echo 'sent';
+} else {
+    // Transfer the value 'failed' to ajax function for showing error message.
+    echo 'failed';
+}
 ?>
+
+
